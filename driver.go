52cbbcdc6e917e43162fe6e5c7a56bbb28e03ea7
hamedwaezi01 <hamedwaezi01@gmail.com>
2023-08-04 16:00:55 -0400

    trapping water problem

--- driver.go
+++ driver.go
@@ -12,7 +12,9 @@ func main() {
 	// driver4SumCount()
 	// driverGenerateParanthesis()
 	// driverNextPermutation()
-	driverPermute()
+	// driverPermute() //???
+	fmt.Println(trap([]int{0,1,0,2,1,0,1,3,2,1,2,1}))
+	// fmt.Println(trap([]int{4,2,3}))
 }
 
 func driverNextPermutation() {
---
+++ trappingwater.go
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/trapping-rain-water/
+package main
+
+import "fmt"
+
+func trap(height []int) int {
+	l := len(height)
+	if l == 1 {
+		return 0
+	}
+	sum := 0
+	notSum := 0
+	prev := 0
+
+	for i:=1; i<l; i++ {
+		if height[i] < height[prev] {
+			notSum += height[i]
+		} else {
+			tmp := (height[i] * (i-prev-1)) - notSum - ((height[i] - height[prev]) * (i-prev-1))
+			sum += tmp
+			notSum = 0
+			prev = i
+		}
+	}
+	newPrev, prevVal := l, 0
+	notSum = 0
+	for i:=l-1; i >= prev; i-- {
+		if height[i] < prevVal {
+			notSum += height[i]
+		} else {
+			tmp := (height[i] * (newPrev-i-1)) - notSum - ((height[i]-prevVal)* (newPrev-i-1))
+			sum += tmp
+			notSum = 0
+			prevVal = height[i]
+			newPrev = i
+		}
+	}
+    return sum
+}
+
